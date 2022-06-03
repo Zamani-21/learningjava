d10d91ecc4c93e7fe8050725c690e5382d5f20fc
Zamani-21 <nyamburan45@gmail.com>
2022-06-03 18:14:20 +0300

    added the @override

---
+++ Area/Shape.java
@@ -0,0 +1,18 @@
+abstract class Shape {
+    public abstract double calculateArea();
+
+}
+
+class Triangle extends Shape {
+    @Override
+    public double calculateArea() {
+        // Triangle implementation
+    }
+
+    class Circle extends Shape {
+        @Override
+        public double calculateArea() {
+            // Circle implementation
+        }
+    }
+}// code not complete
